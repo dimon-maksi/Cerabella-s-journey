44c9762fafc87945f1870bd20dde9a4c5cae629f
Maksymenko Dmytro <dmitriymaksimenko88@gmail.com>
2023-12-25 21:27:19 +0200

    Added Health for a character

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Health : MonoBehaviour
+    {
+        private Animator anim;
+        
+        public int MaxHealth;
+        public int CurrentHealth;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+            CurrentHealth = MaxHealth;
+        }
+
+        public void TakeDamage(int Amount)
+        {
+            CurrentHealth -= Amount;
+            if (CurrentHealth <= 0)
+            {
+                anim.SetBool("IsDeath", true);
+                TeleportPlayerToSpawnpoint();
+            }
+        }
+        
+        void TeleportPlayerToSpawnpoint()
+        {
+            Vector3 newPosition = new Vector3(-0.81f, 0.19f, 0f);
+            transform.position = newPosition;
+            anim.SetBool("IsDeath", false);
+        }
+    }
+}
