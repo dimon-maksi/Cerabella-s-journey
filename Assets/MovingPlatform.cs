83a7646e23904b949e341fe91793470bda42249c
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-12-26 18:34:31 +0200

    Moving platform is done
    *Add skin to the platform
    *Create physics to it
    *Create script that allow player follow
    the platform when he is on it
    *Remove unimportant comments from MovingPlatform script

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -40,19 +40,4 @@ public class MovingPlatform : MonoBehaviour
             Gizmos.DrawLine(platform.transform.position, endPoint.position);
         }
     }
-
-
-    //[SerializeField] private GameObject[] waypoints;
-    //private int currentWaypointIndex = 0;
-    //[SerializeField] private float speed = 2f;
-    //private void Update()
-    //{
-    //    if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
-    //    {
-    //        currentWaypointIndex++;
-    //        if (currentWaypointIndex >= waypoints.Length)
-    //        { currentWaypointIndex = 0; }
-    //    }
-    //    transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
-    //}
 }
