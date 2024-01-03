0373bbbec87a3a924a4ea545b44ea9a02640543a
Maksymenko Dmytro <dmitriymaksimenko88@gmail.com>
2024-01-03 22:11:53 +0200

    Added the trigger zone for flying eye follow

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -10,10 +10,14 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] public Transform target;
     [SerializeField] public float speed = 200f;
     [SerializeField] public float nextWaypointDistance = 3f;
+    [SerializeField] public float triggerRadius = 5f;
+    [SerializeField] public LayerMask playerLayer;
 
     private Path path;
     private int currentWaypoint = 0;
     private bool reachedEndOfPath = false;
+    private bool playerIsFinded = false;
+    
 
     private Seeker seeker;
     private Rigidbody2D rb;
@@ -29,7 +33,7 @@ public class EnemyAI : MonoBehaviour
 
     private void UpdatePath()
     {
-        if (seeker.IsDone() )
+        if (seeker.IsDone())
             seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
     
@@ -42,40 +46,57 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (!playerIsFinded && Physics2D.OverlapCircle(transform.position, triggerRadius, playerLayer))
+        {
+            Debug.Log("Player entered the trigger zone!");
+            playerIsFinded = true;
+        }
+    }
+
     void FixedUpdate()
     {
-        if (path == null)
-            return;
-
-        if (currentWaypoint >= path.vectorPath.Count)
+        if (playerIsFinded)
         {
-            reachedEndOfPath = true;
-            return;
-        }
-        else
-        {
-            reachedEndOfPath = false;
-        }
+            if (path == null)
+                return;
 
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                reachedEndOfPath = true;
+                return;
+            }
+            else
+            {
+                reachedEndOfPath = false;
+            }
+
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            Vector2 force = direction * speed * Time.deltaTime;
         
-        rb.AddForce(force);
+            rb.AddForce(force);
 
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
-        if (distance < nextWaypointDistance)
-        {
-            currentWaypoint++;
-        }
+            if (distance < nextWaypointDistance)
+            {
+                currentWaypoint++;
+            }
 
-        if (force.x >= 0.01f)
-        {
-            enemy.localScale = new Vector3(-1f, 1f, 1f);
+            if (force.x >= 0.01f)
+            {
+                enemy.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (force.x <= -0.01f)
+            {
+                enemy.localScale = new Vector3(1f, 1f, 1f);
+            }
         }
-        else if (force.x <= -0.01f)
-        {
-            enemy.localScale = new Vector3(1f, 1f, 1f);
-        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, triggerRadius);
     }
 }
